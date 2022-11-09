3a65a7d159ba94c4db710bf884bd523372279537
deundeunee <51077100+deundeunee@users.noreply.github.com>
2022-11-09 15:56:29 +0900

    Clean data - remove unwanted commas

--- source/rdbms_project_import_data.sql
+++ source/rdbms_project_import_data.sql
@@ -1,8 +1,10 @@
-# to solve an error using data import wizard
+# To solve an error using data import wizard
 select @@global.sql_mode;
 set @@global.sql_mode = 'NO_ENGINE_SUBSTITUTION';
+# Enable loading local data
+SET GLOBAL local_infile=1;
 
-# create schema for a project
+# Create schema for a project
 create schema project;
 use project;
 # Use 'table data import wizard' with setting.csv and truncate data
@@ -11,7 +13,7 @@ truncate setting;
 # Rename the table
 rename table setting to original_shop_seoul;
 # Import original data from a file, which is cleaned by deleting unwanted commas
-LOAD DATA LOCAL INFILE "C:/Users/kb464/OneDrive - g.skku.edu/2022/2022-2_rdbms/original_shop_seoul.csv"
+LOAD DATA LOCAL INFILE "C:/Users/kb464/OneDrive - g.skku.edu/2022/2022-2_rdbms/rdbms_project/original_shop_seoul.csv"
 INTO TABLE original_shop_seoul
 FIELDS TERMINATED BY ','
 LINES TERMINATED BY '\n'
